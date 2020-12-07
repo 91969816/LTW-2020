adb9b3cdecd98e97e2610a74f733de800d076e41
lehquy <63632418+lehquy@users.noreply.github.com>
2020-12-08 01:48:47 +0700

    fix bug 404

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,4 +9,7 @@ class AdminController extends Controller
     public function index(){
         return view('admin_login');
     }
+    public function show_dashboard(){
+        return view('admin.dashboard');
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,78 +2,28 @@
 @section('content')
 
 <div class="features_items"><!--features_items-->
-						<h2 class="title text-center">Sản Phẩm Bán Chạy Nhất</h2>
+						<h2 class="title text-center">Features Items</h2>
 						<div class="col-sm-4">
 							<div class="product-image-wrapper">
 								<div class="single-products">
 										<div class="productinfo text-center">
-											<img src="imagesCat/meo-tai-cup.jpg" alt="" />
-											<h2>$60</h2>
-											<p>Mèo Tai Cụp</p>
-											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
+											<img src="{{('public/frontend/images/product1.jpg')}}" alt="" />
+											<h2>$56</h2>
+											<p>Easy Polo Black Edition</p>
+											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 										</div>
 										<div class="product-overlay">
 											<div class="overlay-content">
-												<h2>$60</h2>
-												<p>Mèo Tai Cụp</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
+												<h2>$56</h2>
+												<p>Easy Polo Black Edition</p>
+												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 											</div>
 										</div>
 								</div>
 								<div class="choose">
 									<ul class="nav nav-pills nav-justified">
-										<li><a href="#"><i class="fa fa-plus-square"></i>Yêu Thích</a></li>
-										<li><a href="#"><i class="fa fa-plus-square"></i>So Sánh Giá</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="product-image-wrapper">
-								<div class="single-products">
-										<div class="productinfo text-center">
-											<img src="imagesCat/meo-ba-tu-2-2.jpg" alt="" />
-											<h2>$75</h2>
-											<p>Mèo Ba Tư</p>
-											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
-										</div>
-										<div class="product-overlay">
-											<div class="overlay-content">
-												<h2>$75</h2>
-												<p>Mèo Ba Tư</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
-											</div>
-										</div>
-								</div>
-								<div class="choose">
-									<ul class="nav nav-pills nav-justified">
-										<li><a href="#"><i class="fa fa-plus-square"></i>Yêu Thích</a></li>
-										<li><a href="#"><i class="fa fa-plus-square"></i>So Sánh Giá</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="product-image-wrapper">
-								<div class="single-products">
-										<div class="productinfo text-center">
-											<img src="imagesCat/meo-long-ngan-anh.jpg" alt="" />
-											<h2>$40</h2>
-											<p>Mèo Anh Lông Ngắn</p>
-											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
-										</div>
-										<div class="product-overlay">
-											<div class="overlay-content">
-												<h2>$40</h2>
-												<p>Mèo Anh Lông Ngắn</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
-											</div>
-										</div>
-								</div>
-								<div class="choose">
-									<ul class="nav nav-pills nav-justified">
-										<li><a href="#"><i class="fa fa-plus-square"></i>Yêu Thích</a></li>
-										<li><a href="#"><i class="fa fa-plus-square"></i>So Sánh Giá</a></li>
+										<li><a href="#"><i class="fa fa-plus-square"></i>Add to wishlist</a></li>
+										<li><a href="#"><i class="fa fa-plus-square"></i>Add to compare</a></li>
 									</ul>
 								</div>
 							</div>
@@ -81,7 +31,7 @@
 					
                         </div><!--features_items-->
                         <div class="category-tab"><!--category-tab-->
-                            <!-- <div class="col-sm-12">
+                            <div class="col-sm-12">
                                 <ul class="nav nav-tabs">
                                     <li class="active"><a href="#tshirt" data-toggle="tab">T-Shirt</a></li>
                                 </ul>
@@ -104,7 +54,7 @@
                                     
                                 
                                 </div>
-                            </div> -->
+                            </div>
                         </div><!--/category-tab-->
 					
 					<!--CODE DANH MUC SAN PHAM O DAY CATEGORIES -->
@@ -113,7 +63,7 @@
                     <!--CODE DE XUAT SAN PHAM O DAY-->
                     
 					<div class="recommended_items"><!--recommended_items-->
-						<h2 class="title text-center">Đề Xuất Khác</h2>
+						<h2 class="title text-center">recommended items</h2>
 						
 						<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
 							<div class="carousel-inner">
@@ -122,10 +72,10 @@
 										<div class="product-image-wrapper">
 											<div class="single-products">
 												<div class="productinfo text-center">
-													<img src="imagesCat/Meo Maine Coon.jpg" alt="" />
+													<img src="{{('public/frontend/images/recommend1.jpg')}}" alt="" />
 													<h2>$56</h2>
-													<p>Mèo Maine Coon</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
+													<p>Easy Polo Black Edition</p>
+													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 												</div>
 												
 											</div>
@@ -135,10 +85,10 @@
 										<div class="product-image-wrapper">
 											<div class="single-products">
 												<div class="productinfo text-center">
-													<img src="imagesCat/meo long ngan my.jpeg" alt="" />
-													<h2>$30</h2>
-													<p>Mèo Lông Ngắn Mỹ</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
+													<img src="{{('public/frontend/images/recommend1.jpg')}}" alt="" />
+													<h2>$56</h2>
+													<p>Easy Polo Black Edition</p>
+													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 												</div>
 												
 											</div>
@@ -148,10 +98,10 @@
 										<div class="product-image-wrapper">
 											<div class="single-products">
 												<div class="productinfo text-center">
-													<img src="imagesCat/meo bombay.jpg" alt="" />
-													<h2>$96</h2>
-													<p>Mèo BomBay</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
+													<img src="{{('public/frontend/images/recommend1.jpg')}}" alt="" />
+													<h2>$56</h2>
+													<p>Easy Polo Black Edition</p>
+													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 												</div>
 												
 											</div>
@@ -163,50 +113,42 @@
 										<div class="product-image-wrapper">
 											<div class="single-products">
 												<div class="productinfo text-center">
-													<img src="imagesCat/meo long ngan phuong dong.jpg" alt="" />
-													<h2>$35</h2>
-													<p>Mèo Lông Ngắn Phương Đông</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
+													<img src="{{('public/frontend/images/recommend1.jpg')}}" alt="" />
+													<h2>$56</h2>
+													<p>Easy Polo Black Edition</p>
+													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 												</div>
 												
 											</div>
 										</div>
 									</div>
-									<div class="item">	
 									<div class="col-sm-4">
 										<div class="product-image-wrapper">
 											<div class="single-products">
 												<div class="productinfo text-center">
-													<img src="imagesCat/meo cuot duoi my.jpg" alt="" />
-													<h2>$77</h2>
-													<p>Mèo Cuột Đuôi Mỹ</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
+													<img src="{{('public/frontend/images/recommend1.jpg')}}" alt="" />
+													<h2>$56</h2>
+													<p>Easy Polo Black Edition</p>
+													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 												</div>
 												
 											</div>
 										</div>
 									</div>
-									
-								</div>
-								<div class="item">	
 									<div class="col-sm-4">
 										<div class="product-image-wrapper">
 											<div class="single-products">
 												<div class="productinfo text-center">
-													<img src="imagesCat/meo long ngan ba tu.jpg" alt="" />
-													<h2>$85</h2>
-													<p>Mèo Lông Ngắn Ba Tư</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm Vào Giỏ Hàng</a>
+													<img src="{{('public/frontend/images/recommend1.jpg')}}" alt="" />
+													<h2>$56</h2>
+													<p>Easy Polo Black Edition</p>
+													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 												</div>
 												
 											</div>
 										</div>
 									</div>
-									
 								</div>
-									
-								</div>
-								
 							</div>
 							 <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
 								<i class="fa fa-angle-left"></i>
