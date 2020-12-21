68500fa60ace4a71b0538ff3f961e6bfa6f04787
lehquy <63632418+lehquy@users.noreply.github.com>
2020-12-22 00:56:07 +0700

    add models

---
+++ app/Models/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $fillable =[
+        'admin_password','admin_email','admin_name','admin_phone'
+    ];
+    protected $primaryKey = 'admin_id';
+    protected $table ='tbl_admin';
+
+    public function findByEmail($email)
+    {
+        $admin = Admin::where('admin_email',$email)->first();
+        return $admin;
+    }
+
+
+}
